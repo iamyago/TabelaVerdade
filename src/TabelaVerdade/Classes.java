2247af26d5106a25eee75fe3f0e8e27a9ea74344
Yago <yagodev10@gmail.com>
2026-03-26 21:59:14 -0300

    Mat

--- src/TabelaVerdade/Classes.java
+++ src/TabelaVerdade/Classes.java
@@ -4,24 +4,16 @@ import java.util.function.BiFunction;
 import java.util.function.Function;
 
 enum Conectivo implements OperacaoLogica {
-    // Use BiFunction quando a lógica depende de dois valores
-    //
-    //Ex: soma, comparação, combinação de dados
-
-    // Use Function quando você precisa transformar um valor em outro
-    //
-    //Ex: converter, formatar, mapear dados
 
     CONJUNCAO("∧", (p, q) -> p && q),
     DISJUNCAO("∨", (p, q) -> p || q),
     CONDICIONAL("→", (p, q) -> !p || q),
     BICONDICIONAL("↔", (p, q) -> p == q),
-    NEGACAO("¬", p -> !p);
+    NEGACAO("¬", p -> !p);  // ← MUDE AQUI!
 
     public final String simbolo;
     public final BiFunction<Boolean, Boolean, Boolean> operacaoBinaria;
     public final Function<Boolean, Boolean> operacaoUnaria;
-
 
     Conectivo(String simbolo, BiFunction<Boolean, Boolean, Boolean> op) {
         this.simbolo = simbolo;
@@ -34,14 +26,18 @@ enum Conectivo implements OperacaoLogica {
         this.operacaoUnaria = op;
         this.operacaoBinaria = null;
     }
+
     public boolean aplicar(boolean p, boolean q) {
         if (operacaoBinaria != null) {
             return operacaoBinaria.apply(p, q);
         }
-        return operacaoUnaria.apply(p);  // q ignorado
+        throw new IllegalStateException("não aceita 2 parâmetros");
     }
 
     public boolean aplicar(boolean p) {
-        return false;
+        if (operacaoUnaria != null) {
+            return operacaoUnaria.apply(p);
+        }
+        throw new IllegalStateException("não aceita 1 parâmetro");
     }
 }
